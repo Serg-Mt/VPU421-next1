4e51523cb524bceff2ef15aa643957a522c27bae
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2025-12-17 21:28:35 +0400

    todo +form1 +form2

--- components/to-do.jsx
+++ components/to-do.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 class ToDoItem {
   id = Math.random(); // + Date.now();
@@ -14,19 +14,54 @@ class ToDoItem {
   }
 }
 
-function Button({ children, onClick }) {
+const Button =function Button({ children, onClick }) {
+  console.debug('Button render', children);
   return <button onClick={onClick}>{children}</button>
 }
 
-function Form({ addItem }) { // c управляемым input
+function Form0({ addItem }) { // c управляемым input
   const
     [value, setValue] = useState('=start=');
+  console.log('Form 0 render', value);
   return <fieldset>
     <legend>Form 0</legend>
     <input value={value} onInput={event => setValue(event.currentTarget.value)} />
     <Button onClick={() => addItem(value)}>➕</Button>
   </fieldset>
 }
+
+function Form1({ addItem }) { // ref
+  const
+    ref = useRef(null);
+  console.log('Form 1 render', ref);
+  return <fieldset>
+    <legend>Form 1</legend>
+    <input ref={ref} />
+    <Button onClick={() => addItem(ref.current.value)}>➕</Button>
+  </fieldset>
+}
+
+function Form2({ addItem }) { // form
+  console.log('Form 2 render');
+  return <fieldset>
+    <legend>Form 2</legend>
+    <form
+      style={{ backgroundColor: 'azure', padding: '10px' }}
+      onSubmit={event => {
+        event.preventDefault();
+        const
+          formData = new FormData(event.currentTarget);
+        addItem(formData.get("input-name"))
+      }}
+    >
+      <input name="input-name" />
+      <input type='submit' />
+    </form>
+
+  </fieldset>
+}
+
+const Form = [Form0, Form1, Form2][0];
 
 
 
@@ -39,7 +74,6 @@ export function ToDo({ }) {
       const
         index = prev.findIndex(el => id === el.id),
         elem = prev[index];
-
       return prev.with(index, elem.toggleCheckbox());
     });
   return <>
@@ -55,6 +89,7 @@ export function ToDo({ }) {
  * @returns 
  */
 function Item({ item, delItem, toggleCheckbox }) {
+  console.log('Item render', item);
   const
     { id, text, checked } = item;
   return <li>
@@ -74,6 +109,7 @@ function Item({ item, delItem, toggleCheckbox }) {
  * @returns 
  */
 function List({ list, delItem, toggleCheckbox }) {
+  console.debug('List render');
   return <fieldset>
     <legend>List</legend>
     <ol>
